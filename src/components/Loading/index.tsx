8131f817ef5f2d48a6f254300948df5787cff70d
Tundê <ayotunde_sales@hotmail.com>
2021-12-10 00:07:10 -0300

    feat/home: add loadging on route change

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,44 @@
+import { Flex } from "@chakra-ui/react"
+
+import {motion} from "framer-motion"
+
+import {HiCode} from "react-icons/hi"
+
+// import Image from "next/image"
+
+export const Loading = () => {
+  return(
+    <Flex
+      h="100vh"
+      w="100vw"
+      align="center"
+      justify="center"
+    >
+      <motion.div
+        initial="hidden" 
+        animate="visible" 
+        variants={{
+        hidden: {
+          scale: 0.4,
+          opacity: 0
+        },
+        visible: {
+          scale: 1.3,
+          opacity: 1,
+          transition: {
+            delay: 0,
+            duration: 1.6
+          }
+        }
+      }}>
+        {/* <Image 
+            width={420}
+            height={410}
+            alt="Dev"
+            src="/images/tunde.svg"
+          /> */}
+        <HiCode size={45}/>
+      </motion.div>
+    </Flex>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,33 @@
-import {ChakraProvider} from "@chakra-ui/react"
+import { useState } from 'react';
+import {AppProps} from "next/app";
 
+import {ChakraProvider} from "@chakra-ui/react"
 import {SidebarDrawerProvider} from "../hooks/SidebarDrawerContext"
 
-import {AppProps} from "next/app"
+import router from "next/router"
 
 import {theme} from "../styles/theme"
+import { Loading } from "../components/Loading"
 
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false)
+
+  router.events.on('routeChangeStart', (url) => {
+    setLoading(true)
+  })
+
+  router.events.on('routeChangeComplete', (url) => {
+    setLoading(false)
+  })
+
   return (
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
-        <Component {...pageProps} />
+        {
+          loading 
+          ? <Loading /> 
+          : <Component {...pageProps} />
+        }
       </SidebarDrawerProvider>
     </ChakraProvider>
   ) 
