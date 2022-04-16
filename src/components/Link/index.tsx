9bdce713d18fa022ec107790a0399b19d793e317
Tundê <ayotunde_sales@hotmail.com>
2022-04-15 23:04:35 -0300

    fix(home): fix links

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,15 +1,15 @@
-import {HStack} from "@chakra-ui/react"
+import { HStack } from "@chakra-ui/react"
 
-import {LinkComponent} from "./Link"
+import { LinkComponent } from "./Link"
 
 import {
   SiGithub as Github,
   SiLinkedin as Linkedin
 } from "react-icons/si"
 
-import {IoRocketSharp as Rocket} from "react-icons/io5"
+import { IoRocketSharp as Rocket } from "react-icons/io5"
 
-import {AiOutlineMail as Mail} from "react-icons/ai"
+import { AiOutlineMail as Mail } from "react-icons/ai"
 
 
 export function HomePageLinks(): JSX.Element {
@@ -20,22 +20,22 @@ export function HomePageLinks(): JSX.Element {
       align="center"
       justify="flex-end"
     >
-      <LinkComponent href="https://www.linkedin.com/in/tund%C3%AA-cavalcante-1621441ba/">
-        <Linkedin size={23}/>
+      <LinkComponent href="https://www.linkedin.com/in/tund%C3%AA-cavalcante/">
+        <Linkedin size={23} />
       </LinkComponent>
 
       <LinkComponent href="https://github.com/Ceviche9">
-        <Github size={25}/>
+        <Github size={25} />
       </LinkComponent>
 
       <LinkComponent href="https://app.rocketseat.com.br/me/tunde-cavalcante-08588">
-        <Rocket size={30}/>
+        <Rocket size={30} />
       </LinkComponent>
 
       <LinkComponent href="mailto:ayotunde_sales@hotmail.com">
-        <Mail size={30}/>
+        <Mail size={30} />
       </LinkComponent>
-  </HStack>
+    </HStack>
 
   )
 }
